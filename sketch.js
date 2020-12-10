7a6020eb0966f84a235b8edbfafb87a83c7b6656
adithyarajendran <72382374+adithyarajendran@users.noreply.github.com>
2020-12-10 20:57:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,21 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
- 
-var engine,world;
-var ground
+var canvas, backgroundImage;
+var allPlayers;
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var form, player, game;
+var car1,car2,car3 ,car4, cars;
+
 function setup(){
-
-createCanvas(3000,800)
-engine = Engine.create();
-world = engine.world;
-ground = new Ground(600,600,1200,20)
-
+  canvas = createCanvas(displayWidth - 20,displayHeight - 30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-
-ground.display()
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
